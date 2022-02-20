7dcbef04d656a0abad3d66521b8cceb8d1cecf33
Huy Chuong <chuonglh5798@gmail.com>
2022-02-21 02:33:35 +0700

    event ver2

--- src/components/CreateEvent/CreatEvent.tsx
+++ src/components/CreateEvent/CreatEvent.tsx
@@ -1,37 +1,192 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
 import { useStyles } from "./Event.style";
+import { Grid, Typography, Avatar, Dialog, Slide } from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ShareIcon from "@mui/icons-material/Share";
+import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
+import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
 const Event = () => {
   const classes = useStyles();
+  const [openDialog, setOpenDialog] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isLiked, setIsliked] = useState(false);
+
+  const user = {
+    avatar: "",
+    name: "Nguyen Trung Hai",
+  };
+
+  const onClickLike = () => {
+    setIsliked(!isLiked);
+  };
+
+  const onClickCommentSection = () => {
+    setOpenDialog(true);
+  };
+
+  const onClickBookmark = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
+  const onClickCommentSectionInsideDialog = () => {};
+
+  const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+      children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>
+  ) {
+    return <Slide direction="left" ref={ref} {...props} />;
+  });
+
+  const post = {
+    title: "Help me with CSD",
+    time: "20h",
+    content: "help me please",
+    hashtag: "#CSD",
+    isLiked: false,
+    likeCount: 23,
+    commentCount: 5,
+    isBookmarked: false,
+  };
+
+  const onCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const renderPostFullScreen = () => {
+    return (
+      <Dialog keepMounted TransitionComponent={Transition} onClose={onCloseDialog} fullScreen open={openDialog}>
+        <div className={classes.dialogContent}>
+          <Grid container className={classes.dialogHeader}>
+            <Grid onClick={onCloseDialog} xs={2} item className={classes.backBtn}>
+              <ArrowBackIosIcon color="primary" />
+            </Grid>
+            <Grid xs={8} item className={classes.postTitle}>
+              <Typography>{post.title}</Typography>
+            </Grid>
+            <Grid xs={2} item className={classes.moreBtn}>
+              <MoreHorizIcon color="primary" />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.postContent}>
+            <div className={classes.postContent}>
+              <Typography>{post.content}</Typography>
+              <Typography>{post.hashtag}</Typography>
+            </div>
+            <Grid container item xs={12} className={classes.simpleActions}>
+              <Grid item xs={4} className={classes.likeButton} onClick={onClickLike}>
+                {isLiked ? <ThumbUpIcon color="primary" /> : <ThumbUpOutlinedIcon color="primary" />}
+              </Grid>
+              <Grid item xs={4} className={classes.commentButton} onClick={onClickCommentSectionInsideDialog}>
+                <ChatBubbleOutlineOutlinedIcon color="primary" />
+              </Grid>
+              <Grid item xs={4} className={classes.shareButton}>
+                <ShareIcon color="primary" />
+              </Grid>
+            </Grid>
+          </Grid>
+        </div>
+      </Dialog>
+    );
+  };
 
   return (
     <div className={classes.hashtag}>
-      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "7vh"}}>
+      {openDialog && renderPostFullScreen()}
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "7vh" }}>
         <div>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
+            SE
+          </Button>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
+            IS
+          </Button>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
+            MKT
+          </Button>
+        </div>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "7vh" }}>
+        <div>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             Semester I
           </Button>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             Semester II
           </Button>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             Semester III
           </Button>
         </div>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "7vh"}}>
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "7vh" }}>
         <div>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             MAS291
           </Button>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             MAS291
           </Button>
-          <Button variant="outlined" size="small" href="#contained-buttons" style={{color: 'red', margin: 5}}>
+          <Button variant="outlined" size="small" href="#contained-buttons" style={{ color: "red", margin: 5 }}>
             MAS291
           </Button>
         </div>
       </Box>
+      <div className={classes.content}>
+        <Typography>Nổi bật</Typography>
+      </div>
+      <div className={classes.filterByMajor}></div>
+      <div className={classes.listPost}>
+        {[1, 2, 3, 4].map((i) => (
+          <div key={i} className={classes.post}>
+            <Grid container className={classes.userPanel}>
+              <Grid item xs={2} className={classes.userAvatar}>
+                <Avatar src={user?.avatar} />
+              </Grid>
+              <Grid item xs={8} className={classes.userNameAndPostTime}>
+                <Typography>{user?.name}</Typography>
+                <Typography>{post?.time}</Typography>
+              </Grid>
+              <Grid item xs={2} className={classes.userMoreActions}>
+                <MoreHorizIcon color="info" />
+              </Grid>
+            </Grid>
+            <div className={classes.postContent}>
+              <Typography>{post.content}</Typography>
+              <Typography>{post.hashtag}</Typography>
+            </div>
+            <Grid container className={classes.postActions}>
+              <Grid item xs={10} className={classes.leftPanel}>
+                <div className={classes.likeButton} onClick={onClickLike}>
+                  {isLiked ? <ThumbUpIcon color="primary" /> : <ThumbUpOutlinedIcon color="primary" />}
+                  <Typography>{post.likeCount}</Typography>
+                </div>
+                <div className={classes.commentButton} onClick={onClickCommentSection}>
+                  <ChatBubbleOutlineOutlinedIcon color="primary" />
+                  <Typography>{post.commentCount}</Typography>
+                </div>
+                <div className={classes.shareButton}>
+                  <ShareIcon color="primary" />
+                </div>
+              </Grid>
+              <Grid item xs={2} className={classes.rightPanel}>
+                <div className={classes.bookmarkButton} onClick={onClickBookmark}>
+                  {isBookmarked ? <BookmarkAddedIcon color="primary" /> : <BookmarkAddOutlinedIcon color="primary" />}
+                </div>
+              </Grid>
+            </Grid>
+            <div className={classes.divider} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/CreateEvent/Event.style.tsx
+++ src/components/CreateEvent/Event.style.tsx
@@ -4,7 +4,112 @@ export const useStyles: any = makeStyles(
   () => ({
     hashtag: {
 
+    },
+    listPost: {
+      marginTop: 20,
+    },
+    post: {
+      width: "100%",
+      height: "auto",
+      backgroundColor: "#F8F8F8",
+      padding: 15,
+      borderRadius: 8,
+      marginBottom: 16,
+    },
+    userPanel: {
+      "& > div": {
+        "&:first-child": {
+          maxWidth: 43,
+          margin: 0,
+          borderRadius: "50%",
+          border: "1px solid #0288d1",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        "&:nth-child(2)": {
+          marginLeft: 10,
+        },
+        display: "flex",
+        flexGrow: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        "&:last-child": {
+          alignItems: "flex-end",
+        },
+      },
+    },
+    userAvatar: {
+      width: 43,
+      height: 43,
+    },
+    userNameAndPostTime: {
+      display: "flex",
+      alignItems: "flex-start",
+      justifyContent: "center",
+      "& > p:nth-child(1)": {
+        fontSize: 16,
+        lineHeight: "16px",
+        letterSpacing: -0.2,
+        fontWeight: 700,
+        color: "#0288d1",
+      },
+      "& > p:nth-child(2)": {
+        marginTop: 4,
+        fontSize: 12,
+        lineHeight: "16px",
+        letterSpacing: -0.2,
+        fontWeight: 400,
+        color: "#727477",
+      },
+    },
+    content: {
+      width: "100%",
+      "& > p:first-child": {
+        fontSize: 18,
+        fontWeight: 700,
+        textAlign: "left",
+        color: "#0288d1",
+      },
+    },
+    postContent: {
+      width: "100%",
+      height: "auto",
+      marginTop: 10,
+      "& > p": {
+        textAlign: "left",
+      },
+    },
+    postActions: {
+      marginTop: 30,
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      "& > div": {
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        "&:last-child": {
+          justifyContent: "flex-end",
+        },
+        "& > div": {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          marginRight: 20,
+          "&:last-child": {
+            margin: 0,
+          },
+          "& > p": {
+            marginLeft: 6,
+          },
+        },
+      },
     }
   }),
   { index: 1 }
+
+  
 );
