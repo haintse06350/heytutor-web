7bd5f104ebb8c3e556a5c53f50d4cbd5b739c2ef
Cao DucAnh <57619555+caoducanh280698@users.noreply.github.com>
2022-02-21 12:38:37 +0700

    feat: chat-fix reponsive home

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BottomNav from "./components/BottomNavigation/BottomNavigation";
-// import Dashboard from "./components/Dashboard/Dashboard";
+import Chat from "./components/Chat/Chat";
+import Dashboard from "./components/Dashboard/Dashboard";
 import HomePage from "./components/HomePage/HomePage";
+// import Message from "./components/Message/Message";
 // import Login from "./components/Login/Login";
 import Post from "./components/Post/Post";
 import UserProfile from "./components/UserProfile/UserProfile";
@@ -21,6 +23,8 @@ export default function AppRoutes() {
           <Route path={"/home"} element={<HomePage />} />
           <Route path={"/post"} element={<Post />} />
           <Route path={"/profile"} element={<UserProfile />} />
+          <Route path={"/test"} element={<Chat />} />
+          <Route path={"/dashboard"} element={<Dashboard />} />
 
           {/* <Route path={"/profile"} element={!user ? <Login /> : user.isAdmin ? <Dashboard /> : <UserProfile />} /> */}
           {/* </GlobalContextProvider> */}
